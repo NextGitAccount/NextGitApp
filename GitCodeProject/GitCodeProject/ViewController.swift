cdbe1bf3d89e272373de6e3735aac83af49d1768
students <students@Next.local>
2016-04-28 23:20:23 -0700

    创建新的项目

---
+++ GitCodeProject/GitCodeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitCodeProject
+//
+//  Created by students on 16/4/28.
+//  Copyright (c) 2016年 MN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
